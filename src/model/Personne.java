7c9241e754cbc91d75317ecca9af1ce41d99ccbd
Doryan Vousemer <dory.vousemer@gmail.com>
2026-05-12 08:31:03 -0400

    Ajout de la classe Personne

--- src/model/Personne.java
+++ src/model/Personne.java
@@ -1,4 +1,29 @@
 package model;
 
 public abstract class Personne {
+    private String nom;
+    private String prenom;
+    private String id;
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getId() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return "Patient [nom=" + nom + ", prenom=" + prenom + "]";
+
+    }
 }
